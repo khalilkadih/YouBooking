44e9e947ded6bc0d976a1559ba8e251e13ca0d89
khalil el kadih <khalil94elkadih@gmail.com>
2023-01-30 21:22:54 +0000

    test  jenkins 4

--- src/main/java/com/youcode/youbooking/Controller/AdminController.java
+++ src/main/java/com/youcode/youbooking/Controller/AdminController.java
@@ -40,7 +40,7 @@ public class AdminController {
     @GetMapping("/hi")
     public String siHello(){
         String hi="say hello";
-        return "hello from controllers is just for test jenkins"+hi;
+        return "hello from controllers is just for test jenkins";
 
     }
 
